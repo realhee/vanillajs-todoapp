58d4a1a4b96c4c9a8b4689e468325ba4b01ddf49
realhee <wlsgml5628@gmail.com>
2020-10-15 17:36:19 +0900

    4. TodoList 객체화

--- todo.js
+++ todo.js
@@ -2,17 +2,32 @@ const toDoForm = document.querySelector(".js-toDoForm"),
   toDoInput = toDoForm.querySelector("input"),
   toDoList = document.querySelector(".js-toDoList");
 
-  const TODOS_LS = "toDos";
+const TODOS_LS = "toDos";
+
+const toDos = [];
+
+  function saveToDos() {
+    localStorage.setItem(TODOS_LS, JSON.stringify(toDos));
+  }
+
   
   function paintToDo(text) {
     const li = document.createElement("li");
     const delBtn = document.createElement("button");
+    const span = document.createElement("span");    
+    const newId = toDos.length + 1;
     delBtn.innerText = "❌";
-    const span = document.createElement("span");
     span.innerText = text;
     li.appendChild(delBtn);
-    li.appendChild(span);
-    toDoList.appendChild(li);
+    li.appendChild(span);    
+    li.id = newId;
+    toDoList.appendChild(li);    
+    const toDoObj = {
+        text: text,
+        id: newId
+    };
+    toDos.push(toDoObj);
+    saveToDos();
   }
   
   function handleSubmit(event) {
@@ -22,15 +37,19 @@ const toDoForm = document.querySelector(".js-toDoForm"),
     toDoInput.value = "";
   }
 
-    function loadToDos() {
-      const toDos = localStorage.getItem(TODOS_LS);
-      if (toDos !== null) {
-      }
+  function loadToDos() {
+    const loadedToDos = localStorage.getItem(TODOS_LS);
+    if (loadedToDos !== null) {
+        const parsedToDos = JSON.parse(loadedToDos);
+        parsedToDos.forEach(function(toDo) {
+        paintToDo(toDo.text);
+        });
     }
+  }
     
-    function init() {
-      loadToDos();
-      toDoForm.addEventListener("submit", handleSubmit);
-    }
+  function init() {
+    loadToDos();
+    toDoForm.addEventListener("submit", handleSubmit);
+  }
 
-    init();
+init();
